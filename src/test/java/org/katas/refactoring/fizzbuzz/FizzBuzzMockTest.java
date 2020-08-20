e3ce9f2bd6ed7e2290d76f06926d5a43007d00ef
Songbai Yan <colorguitar@hotmail.com>
2020-08-20 14:47:42 +0800

    [Mock] add mock test case.

--- src/test/java/org/katas/refactoring/fizzbuzz/FizzBuzzMockTest.java
+++ src/test/java/org/katas/refactoring/fizzbuzz/FizzBuzzMockTest.java
@@ -7,11 +7,27 @@ import java.io.PrintStream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class FizzBuzzMockTest {
+
+    @Test
+    public void should_send_input_to_mock() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        PrintStream out = mock(PrintStream.class);
+
+        FizzBuzz.application(fizzBuzz, out, 3);
+
+        verify(out).println("1");
+        verify(out).println("2");
+        verify(out).println("Fizz");
+
+        verify(out, times(3)).println(anyString());
+    }
 
     @Test
     public void should_get_output_from_stub() {
