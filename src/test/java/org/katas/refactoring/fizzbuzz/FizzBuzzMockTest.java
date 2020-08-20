150067f5254d7efd7337ae36a7866b4e95cd78c8
Songbai Yan <colorguitar@hotmail.com>
2020-08-20 14:45:08 +0800

    [Stub] add stub test case.

--- src/test/java/org/katas/refactoring/fizzbuzz/FizzBuzzMockTest.java
+++ src/test/java/org/katas/refactoring/fizzbuzz/FizzBuzzMockTest.java
@@ -8,9 +8,27 @@ import java.io.PrintStream;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class FizzBuzzMockTest {
+
+    @Test
+    public void should_get_output_from_stub() {
+        FizzBuzz fizzBuzz = mock(FizzBuzz.class);
+        PrintStream out = mock(PrintStream.class);
+
+        when(fizzBuzz.sayIt(1)).thenReturn("1");
+        when(fizzBuzz.sayIt(2)).thenReturn("haha");
+        when(fizzBuzz.sayIt(3)).thenReturn("Fizz");
+
+        FizzBuzz.application(fizzBuzz, out, 3);
+
+        verify(out).println("1");
+        verify(out).println("haha");
+        verify(out).println("Fizz");
+    }
+
     @Test
     public void should_print_as_fake() {
 
