90041e3a69754ac95528ab0ce3f621f4386c2390
Songbai Yan <colorguitar@hotmail.com>
2020-08-20 14:02:26 +0800

    [旧的再见] Remove old dividedBy357.

--- src/main/java/org/katas/refactoring/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/katas/refactoring/fizzbuzz/FizzBuzz.java
@@ -4,11 +4,11 @@ package org.katas.refactoring.fizzbuzz;
 public class FizzBuzz {
 
     public String sayIt(int input) {
-        String result = "";
+        String result;
 
         if (String.valueOf(input).contains("3")) return "Fizz";
 
-        result = dividedBy357New(input);
+        result = dividedBy357(input);
 
         if (!"".equals(result)) {
             return result;
@@ -16,7 +16,7 @@ public class FizzBuzz {
         return String.valueOf(input);
     }
 
-    private String dividedBy357New(int input) {
+    private String dividedBy357(int input) {
         String result = "";
         if (input % 3 == 0) {
             result += "Fizz";
@@ -26,23 +26,6 @@ public class FizzBuzz {
         }
         if (input % 7 == 0) {
             result += "Whizz";
-        }
-        return result;
-    }
-
-    private String dividedBy357(int input, String result) {
-        int flag = 0;
-        flag += input % 3 == 0 ? 0b001 : 0;
-        flag += input % 5 == 0 ? 0b010 : 0;
-        flag += input % 7 == 0 ? 0b100 : 0;
-
-        String[] items = {"Fizz", "Buzz", "Whizz"};
-        int i = 0;
-        while (i < 3) {
-            if (((flag >> i) & 1) == 1) {
-                result += items[i];
-            }
-            i++;
         }
         return result;
     }
