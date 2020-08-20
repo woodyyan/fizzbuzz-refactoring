705cad103143c56a5d9f663e0a398d83c0eceb31
Songbai Yan <colorguitar@hotmail.com>
2020-08-20 13:39:40 +0800

    rename test class.

--- src/test/java/org/katas/refactoring/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/org/katas/refactoring/fizzbuzz/FizzBuzzTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class FizzBuzzSmokeTest {
+public class FizzBuzzTest {
 
     @Test
     public void should_return_fizz_when_fizz_buzz_given_input_contains_3() {
